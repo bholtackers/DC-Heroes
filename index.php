c710f28d9ca055c82c066a90cf53190cb0c0e631
Bram <bholtackers@outlook.com>
2017-12-19 00:26:17 +0100

    Changes made to style.css and index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php include 'inc/Connection.php';
+
+if (isset($_GET['teamId'])) {
+    $teamId = $_GET['teamId'];
+    $sql ="SELECT * FROM hero Where teamId='".$teamId."'";
+} else {
+    $sql ="SELECT * FROM hero where teamId=1";
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,8 +20,36 @@
 	<title>DC Heroes</title>
 </head>
 <body>
-  <header id="header">
-    <img id="header-logo" src="img/Logo.jpg">
-      <h1>Heroes</h1>
-  </header>
+	<?php include 'inc/Header.php'; ?>
+	<div id="main-container">
+		<div id="menu-container">
+			<h2>Teams</h2>
+				<?php include 'inc/Menu.php'; ?>
+		</div>
+
+		<div id="hero-container">
+						<?php
+                        $result = $conn->query($sql);
+
+                        if ($result->num_rows > 0) {
+                            while ($row = $result->fetch_assoc()) {
+                                ?>
+			<div id="heroes">
+				<img id="hero-image" src= <?php echo $row['heroImage']; ?> >
+				<p id="hero-name"> <?php echo $row['heroName']; ?></p>
+				<p id="hero-desc"> <?php echo $row['heroDescription']; ?></p>
+			</div>
+			<?php
+                            }
+                        } else {
+                            echo "0 results";
+                        } ?>
+		</div>
+
+		<div id="info-container">
+		</div>
+
+	</div>
+	<?php include 'inc/Footer.php'; ?>
 </body>
+</html>

9cdc28fcf28beee7fd169100543ef88325065671
Bram <bholtackers@outlook.com>
2017-12-18 15:00:27 +0100

    First lesson summary

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="DC Heroes">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>DC Heroes</title>
+</head>
+<body>
+  <header id="header">
+    <img id="header-logo" src="img/Logo.jpg">
+      <h1>Heroes</h1>
+  </header>
+</body>

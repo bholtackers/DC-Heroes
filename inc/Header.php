fde4fd117303318278b63fd21f36decff4dde89b
Bram <bholtackers@outlook.com>
2017-12-19 00:25:45 +0100

    Some php files to include

---
+++ inc/Header.php
@@ -0,0 +1,7 @@
+<header id="header">
+	<a href="index.php">
+    <img id="header-logo" src="img/Logo.jpg" >
+      <h1>Heroes</h1>
+  </a>
+    <img id="header-heroes" src="img/Heroes.jpg">
+</header>
---
+++ inc/Menu.php
@@ -0,0 +1,10 @@
+<nav id="main-nav">
+  <ul>
+    <li><a href="index.php?teamId=1">Justice League </a></li>
+    <li><a href="index.php?teamId=2">Flash Family</a></li>
+    <li><a href="index.php?teamId=3">Batman and Robin</a></li>
+    <li><a href="index.php?teamId=4">Teen Titans</a></li>
+    <li><a href="index.php?teamId=5">Team Arrow</a></li>
+    <li><a href="index.php?teamId=6">Titans</a></li>
+  </ul>
+</nav>

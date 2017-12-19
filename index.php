63a1b4b250c87856b6869053da2817612a6cc9ad
Bram <bholtackers@outlook.com>
2017-12-19 10:33:56 +0100

    Some more changes to style.css and index.php

--- index.php
+++ index.php
@@ -7,6 +7,19 @@ if (isset($_GET['teamId'])) {
     $sql ="SELECT * FROM hero where teamId=1";
 }
 
+if (isset($_GET['heroId'])) {
+    $heroId = $_GET['heroId'];
+    $sql2 ="SELECT * FROM hero Where heroId='".$heroId."'";
+} else {
+    $sql2 ="SELECT * FROM hero where heroId=0";
+}
+
+if (isset($_GET['teamId'])) {
+    $teamId = $_GET['teamId'];
+    $sql3 ="SELECT * FROM team Where teamId='".$teamId."'";
+} else {
+    $sql3 ="SELECT * FROM team where teamId=1";
+}
 
 
 ?>
@@ -22,9 +35,22 @@ if (isset($_GET['teamId'])) {
 <body>
 	<?php include 'inc/Header.php'; ?>
 	<div id="main-container">
+
 		<div id="menu-container">
 			<h2>Teams</h2>
 				<?php include 'inc/Menu.php'; ?>
+        <?php
+                    $result3 = $conn->query($sql3);
+
+                    if ($result3->num_rows > 0) {
+                        while ($row3 = $result3->fetch_assoc()) {
+                            ?>
+  			<img id="teamImage" src= <?php echo $row3['teamImage']; ?> >
+        <?php
+                        }
+                    } else {
+                        echo "Bamboozeled";
+                    } ?>
 		</div>
 
 		<div id="hero-container">
@@ -37,7 +63,10 @@ if (isset($_GET['teamId'])) {
 			<div id="heroes">
 				<img id="hero-image" src= <?php echo $row['heroImage']; ?> >
 				<p id="hero-name"> <?php echo $row['heroName']; ?></p>
-				<p id="hero-desc"> <?php echo $row['heroDescription']; ?></p>
+				<p id="hero-desc1"> <?php echo $row['heroDescription']; ?></p> </br>
+        <a href="index.php?heroId=<?php echo $row['heroId']; ?>">
+        <nav id="info">info</nav>
+      </a>
 			</div>
 			<?php
                             }
@@ -47,6 +76,23 @@ if (isset($_GET['teamId'])) {
 		</div>
 
 		<div id="info-container">
+      <?php
+                  $result2 = $conn->query($sql2);
+
+                  if ($result2->num_rows > 0) {
+                      while ($row2 = $result2->fetch_assoc()) {
+                          ?>
+                          <h2> <?php echo $row2['heroName']; ?></h2> </br>
+                          <h3>Info</h3>
+                          <p class="hero-desc2"> <?php echo $row2['heroDescription']; ?></p>
+                          <h3>Powers</h3>
+                          <p class="hero-desc2"> <?php echo $row2['heroPower']; ?></p>
+<?php
+                      }
+                  } else {
+                      ?>
+                    <h2> <?php echo "Please select a superhero." ?></h2> <?php
+                  } ?>
 		</div>
 
 	</div>

0b03b64c3ea03dccea2f2f04733f28ea50072e88
Bram <bholtackers@outlook.com>
2017-12-19 10:33:37 +0100

    Added more teams

--- inc/Menu.php
+++ inc/Menu.php
@@ -6,5 +6,7 @@
     <li><a href="index.php?teamId=4">Teen Titans</a></li>
     <li><a href="index.php?teamId=5">Team Arrow</a></li>
     <li><a href="index.php?teamId=6">Titans</a></li>
+    <li><a href="index.php?teamId=7">Gen 13</a></li>
+    <li><a href="index.php?teamId=8">Suicide Squad</a></li>
   </ul>
 </nav>

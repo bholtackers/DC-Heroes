b469578c33e99ccaf64e4721bbe3ffad2a4e00d2
Bram <bholtackers@outlook.com>
2017-12-21 23:18:25 +0100

    Some more added stuff to both code pages

--- inc/Header.php
+++ inc/Header.php
@@ -1,5 +1,5 @@
 <header id="header">
-	<a href="index.php">
+	<a href="index.php?teamId=1&heroId=1">
     <img id="header-logo" src="img/Logo.jpg" >
       <h1>Heroes</h1>
   </a>
--- index.php
+++ index.php
@@ -29,6 +29,7 @@ if (isset($_GET['teamId'])) {
 <head>
 	<meta charset="UTF-8">
 	<meta name="description" content="DC Heroes">
+  <link rel="shortcut icon" href="img/DC.ico">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<title>DC Heroes</title>
 </head>
@@ -87,6 +88,28 @@ if (isset($_GET['teamId'])) {
                           <p class="hero-desc2"> <?php echo $row2['heroInfo']; ?></p>
                           <h3>Powers</h3>
                           <p class="hero-desc2"> <?php echo $row2['heroPower']; ?></p>
+                          <form method="POST" class="form-rate">
+    <div class="rate">
+      <input type="radio" id="rating10" name="rating" value="10" /><label class="lblRating" for="rating10" title="5 stars"></label>
+        <input type="radio" id="rating9" name="rating" value="9" /><label class="lblRating half" for="rating9" title="4 1/2 stars"></label>
+      <input type="radio" id="rating8" name="rating" value="8" /><label class="lblRating" for="rating8" title="4 stars"></label>
+        <input type="radio" id="rating7" name="rating" value="7" /><label class="lblRating half" for="rating7" title="3 1/2 stars"></label>
+      <input type="radio" id="rating6" name="rating" value="6" /><label class="lblRating" for="rating6" title="3 stars"></label>
+        <input type="radio" id="rating5" name="rating" value="5" /><label class="lblRating half" for="rating5" title="2 1/2 stars"></label>
+      <input type="radio" id="rating4" name="rating" value="4" /><label class="lblRating" for="rating4" title="2 stars"></label>
+        <input type="radio" id="rating3" name="rating" value="3" /><label class="lblRating half" for="rating3" title="1 1/2 stars"></label>
+      <input type="radio" id="rating2" name="rating" value="2" /><label class="lblRating" for="rating2" title="1 star"></label>
+        <input type="radio" id="rating1" name="rating" value="1" /><label class="lblRating half" for="rating1" title="1/2 star"></label>
+      <input type="radio" id="rating0" name="rating" value="0" /><label class="lblRating" for="rating0" title="No star"></label>
+    </div>
+    <div class="review">
+      <textarea class="formMessage" name="review" placeholder="Please write a review for the hero"></textarea>
+    </div>
+    <div class="rate-submit">
+      <input class="submit-btn" type="submit" name="submitRating" value="Rate Hero"/>
+      <input type="hidden" name="heroId" value=""/>
+    </div>
+</form>
 <?php
                       }
                   } else {

10d8fbc1ca2deba9def43efb5d428557b14cfa0c
Bram <bholtackers@outlook.com>
2018-01-09 20:56:37 +0100

    Finally made it possible to loop the team names

--- inc/Menu.php
+++ inc/Menu.php
@@ -1,11 +1,18 @@
+
+
 <nav id="main-nav">
   <ul>
-    <li><a href="index.php?teamId=1">Justice League - 11</a></li>
-    <li><a href="index.php?teamId=2">Flash Family - 5</a></li>
-    <li><a href="index.php?teamId=3">Batman & Robin - 2</a></li>
-    <li><a href="index.php?teamId=4">Teen Titans - 8</a></li>
-    <li><a href="index.php?teamId=5">Team Arrow - 5</a></li>
-    <li><a href="index.php?teamId=6">Gen 13 - 9</a></li>
-    <li><a href="index.php?teamId=7">Suicide Squad - 7</a></li>
+    <?php
+                $result6 = $conn->query($sql6);
+
+                if ($result6->num_rows > 0) {
+                    while ($row6 = $result6->fetch_assoc()) {
+                        ?>
+    <li><a href="index.php?teamId=<?php echo $row6['teamId'] ?>"><?php echo $row6['teamName']?> - <?php echo $row6['teamMembers']?></a></li>
+    <?php
+                    }
+                } else {
+                    echo "Bamboozeled";
+                } ?>
   </ul>
 </nav>

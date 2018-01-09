33d2441e4b5d333f3a2c4eb2cc5d4a10c1db2175
Bram <bholtackers@outlook.com>
2018-01-09 09:47:09 +0100

    made an alternating background color for the hero divs

--- inc/Menu.php
+++ inc/Menu.php
@@ -1,11 +1,11 @@
-<nav id="main-nav"> 
+<nav id="main-nav">
   <ul>
-    <li><a href="index.php?teamId=1">Justice League </a></li>
-    <li><a href="index.php?teamId=2">Flash Family</a></li>
-    <li><a href="index.php?teamId=3">Batman and Robin</a></li>
-    <li><a href="index.php?teamId=4">Teen Titans</a></li>
-    <li><a href="index.php?teamId=5">Team Arrow</a></li>
-    <li><a href="index.php?teamId=6">Gen 13</a></li>
-    <li><a href="index.php?teamId=7">Suicide Squad</a></li>
+    <li><a href="index.php?teamId=1">Justice League - 11</a></li>
+    <li><a href="index.php?teamId=2">Flash Family - 5</a></li>
+    <li><a href="index.php?teamId=3">Batman & Robin - 2</a></li>
+    <li><a href="index.php?teamId=4">Teen Titans - 8</a></li>
+    <li><a href="index.php?teamId=5">Team Arrow - 5</a></li>
+    <li><a href="index.php?teamId=6">Gen 13 - 9</a></li>
+    <li><a href="index.php?teamId=7">Suicide Squad - 7</a></li>
   </ul>
 </nav>

8c90549b99ead251ace0cf9c884ad05b817e3d7e
Bram <bholtackers@outlook.com>
2018-01-10 21:41:57 +0100

    Put the file in a different place considering it wasn't getting included. also fixed it so it isn't a bland page and just looks nice.

--- Insert.php
+++ Insert.php
@@ -1,5 +1,5 @@
 <?php
-include 'Connection.php';
+include 'inc/Connection.php';
 
 $hero = $_POST['heroId'];
 $team = $_POST['teamId'];
@@ -10,9 +10,21 @@ $timestamp = strtotime($date);
 $sql5 =  "INSERT INTO rating (heroId, rating, ratingDate, ratingReview) VALUES ('$hero', '$rating','$date', '$review')";
 
 if ($conn->query($sql5) === true) {
-    echo "The hero has been rated sucesfully";
+    echo "";
 } else {
     echo "Error: " . $sql5 . "<br>" . $conn->error;
+    die();
 }
 
-header("refresh:1; url=../index.php?teamId=$team&heroId=$hero");
+
+header("refresh:2; url=index.php?teamId=$team&heroId=$hero");
+?>
+
+<html>
+<?php include 'inc/Head.php';?>
+<body>
+  <?php include 'inc/Header.php'; ?>
+    <h1>The hero has been rated succesfully!</h1>
+  <?php include 'inc/Footer.php'; ?>
+</body>
+</html>

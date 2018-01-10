514f5e5b0ed930132336b316a43e9edaa874aa5d
Bram <bholtackers@outlook.com>
2018-01-10 14:41:44 +0100

    Fixed the review page so the time is in correct format

--- inc/Insert.php
+++ inc/Insert.php
@@ -5,15 +5,19 @@ $hero = $_POST['heroId'];
 $rating = $_POST['rating'];
 $review = $_POST['review'];
 $date = (strftime("%Y. %B %d. %A. %X"));
+
+$date  = time();
+
 $timestamp = strtotime($date);
 
 
-$sql5 =  "INSERT INTO rating (heroId, rating, ratingDate, ratingReview) VALUES ('$hero', '$rating', NOW(), '$review')";
+$sql5 =  "INSERT INTO rating (heroId, rating, ratingDate, ratingReview) VALUES ('$hero', '$rating','$date', '$review')";
 
 if ($conn->query($sql5) === true) {
     echo "The hero has been rated sucesfully";
 } else {
     echo "Error: " . $sql5 . "<br>" . $conn->error;
 }
+
 
 header("refresh:1; url=../index.php");

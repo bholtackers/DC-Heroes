6c9c9e6698f97aae382c7795654740d21e1c63d3
Bram <bholtackers@outlook.com>
2018-01-10 21:15:05 +0100

    made the head an includable file

---
+++ inc/Head.php
@@ -0,0 +1,7 @@
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="DC Heroes">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<title>DC Heroes</title>
+</head>
